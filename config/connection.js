484454050af5cbd94e4094fc0d2225ddec296550
DAVID TRUJILLO <davot098@gmail.com>
2024-06-14 15:19:15 -0500

    Prepare to deploy

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,27 @@
 const pgPromise = require ('pg-promise');
+require('dotenv').config();
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
 const config = {
-    host: 'localhost',
-    port: '5432',
-    database: 'utndb',
-    user: 'postgres',
-    password: '200113'
+  host: PGHOST,
+  database: PGDATABASE,
+  user: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: true,
 };
+
 const pgp = pgPromise({});
 const db = pgp(config);
 
 exports.db = db;
+
+// const config = {
+//     host: 'localhost',
+//     port: '5432',
+//     database: 'utndb',
+//     user: 'postgres',
+//     password: '200113'
+// };
+
